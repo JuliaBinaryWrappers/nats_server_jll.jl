82d500d12a3e6969e1d98228fa32025f7687f5a5
jlbuild <juliabuildbot@gmail.com>
2026-08-03 14:22:31 +0000

    nats_server_jll build 2.14.3+0

--- src/nats_server_jll.jl
+++ src/nats_server_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("nats_server")
-JLLWrappers.@generate_main_file("nats_server", UUID("8b1691f7-6002-537d-bdf7-c6203599dc08"))
+JLLWrappers.@generate_main_file("nats_server", Base.UUID("8b1691f7-6002-537d-bdf7-c6203599dc08"))
 end  # module nats_server_jll
